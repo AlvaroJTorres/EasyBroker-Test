ba4cf64ad4d1e117a2208ba4d4674cab17f9f82f
AlvaroJTorres <mangetrone@gmail.com>
2022-01-03 17:43:12 -0500

    Added show controller to be able to see the data of an especific property

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -2,10 +2,13 @@ class PropertiesController < ApplicationController
   before_action :set_client
 
   def index
-    @results = @client.properties.search(limit: 15, search: { statuses: [:published] })
-    @results.each do |property|
-      puts property.title
-    end
+    results = @client.properties.search(limit: 15, search: { statuses: [:published] })
+    @properties = results.response.content
+  end
+
+  def show
+    @property = @client.properties.find(params[:id])
+    p @property
   end
 
   private
