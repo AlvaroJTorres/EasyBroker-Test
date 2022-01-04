e244c05696c933809703a896b97bebad825ed2a2
AlvaroJTorres <mangetrone@gmail.com>
2022-01-04 18:02:12 -0500

    Added the create_contact custom controller to handle te API call for creating a new contact in the contact request endpoint

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,19 +1,45 @@
+require 'httparty'
+
 class PropertiesController < ApplicationController
   before_action :set_client
 
   def index
-    results = @client.properties.search(limit: 15, search: { statuses: [:published] })
-    @properties = results.response.content
+    @results = @client.properties.search(limit: 15, search: { statuses: [:published] })
+    @properties = @results.response.content
   end
 
   def show
     @property = @client.properties.find(params[:id])
-    p @property
+  end
+
+  def create_contact
+    contact_options = {
+      headers: {
+        'Content-Type' => 'application/json',
+        'X-Authorization' => ENV['API_KEY']
+      },
+      body: define_body(params).to_json
+    }
+
+    @response = HTTParty.post('https://api.stagingeb.com/v1/contact_requests', contact_options)
+
+    redirect_to property_path(params[:id])
   end
 
   private
 
   def set_client
     @client = EasyBroker.client
+  end
+
+  def define_body(params)
+    {
+      name: params[:name],
+      phone: params[:phone],
+      email: params[:email],
+      property_id: params[:id],
+      message: params[:message],
+      source: params[:source]
+    }
   end
 end
